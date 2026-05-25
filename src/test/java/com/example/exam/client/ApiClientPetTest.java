519ba15f9d4ef2767af1453980a7f912d7d45129
Jose Alberto Martinez Hernandez <albertmtzh03@gmail.com>
2026-05-25 01:11:15 -0600

    feat(ApiClientPet): Se agrega pruebas unitarias

---
+++ src/test/java/com/example/exam/client/ApiClientPetTest.java
@@ -0,0 +1,66 @@
+package com.example.exam.client;
+
+import com.example.exam.bean.Parametria;
+import com.example.exam.entity.Pet;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApiClientPetTest {
+
+    private Pet testPet = new Pet(1L,"Firulais","available");
+
+    @Test
+    void testApiConnectorExternal() {
+
+        WebClient mockClient = Mockito.mock(WebClient.class, Mockito.RETURNS_DEEP_STUBS);
+
+
+        Mockito.when(mockClient.get()
+                        .uri("/pet/{id}", 1L)
+                        .retrieve()
+                        .bodyToMono(Pet.class))
+                .thenReturn(Mono.just(testPet));
+
+
+
+        assertNotNull(testPet);
+        assertEquals("Firulais", testPet.getName());
+
+
+
+
+    }
+
+
+        @Test
+        void testApiConnectorExternalCreate() {
+
+            WebClient mockClient = Mockito.mock(WebClient.class, Mockito.RETURNS_DEEP_STUBS);
+
+
+
+            Mockito.when(mockClient.post()
+                            .uri("/pet")
+                            .bodyValue(Mockito.any(Pet.class))
+                            .retrieve()
+                            .bodyToMono(Pet.class))
+                    .thenReturn(Mono.just(testPet));
+
+            Parametria mockParametria = Mockito.mock(Parametria.class);
+            Mockito.when(mockParametria.getEndPoint()).thenReturn("http://prueba-api");
+
+            ApiClientPet client = new ApiClientPet(mockParametria);
+
+
+            Pet response = testPet;
+            assertNotNull(response);
+            assertEquals("Firulais", response.getName());
+
+    }
+}
